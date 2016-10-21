ab27d86ee0788a7b760db82e8558291192d13296
Xu Liangwei <xlwyes@pku.edu.cn>
2016-10-21 14:46:16 +0800

    add id & password as arguments

--- connect.py
+++ connect.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
 import urllib.request
 import re
-def connect(username, password, *,  irange='2', action='connect'):
-	url = 'https://its.pku.edu.cn:5428/ipgatewayofpku?uid={username}&password={password}&range={irange}&operation={action}&timeout=1'\
-	.format(username=username, password=password, irange=irange, action=action)
-	print(url)
-	response = urllib.request.urlopen(url)
-	data = response.read().decode('GB18030')
-	print(data)
-	g = re.search('IPGWCLIENT_START (.*) IPGWCLIENT_END', data)
-	msg = g.group(1)
-	return msg
+import argparse
+import getpass
+
+
+def connect(username, password, *, irange='2', action='connect'):
+    url = 'https://its.pku.edu.cn:5428/ipgatewayofpku?uid={username}&password={password}&range={irange}&operation={action}&timeout=1' \
+        .format(username=username, password=password, irange=irange, action=action)
+    # print(url)
+    response = urllib.request.urlopen(url, timeout=1)
+    data = response.read().decode('GB18030')
+    # print(data)
+    g = re.search('IPGWCLIENT_START (.*) IPGWCLIENT_END', data)
+    msg = g.group(1)
+    return msg
+
 
 if __name__ == '__main__':
-	username = '1601214567'
-	password = '*'
-	ret = connect(username, password)
-	print(ret)
+    parser = argparse.ArgumentParser(description='auto-connect')
+    parser.add_argument('-u', '--id', metavar='ID', type=str, help='set the id.')
+    parser.add_argument('-w', '--password', metavar='PASSWORD', type=str, help='set the password.')
+    args = parser.parse_args()
+    if args.id:
+        id = args.id
+        if args.password:
+            password = args.password
+        else:
+            password = getpass.getpass('Input your password:')
+    else:
+        id = input('Input your id:')
+        password = getpass.getpass('Input your password:')
 
+    ret = connect(id, password)
+    print(ret)
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
-import time
-import sys
-import os
-import signal
 import argparse
+import os
+import sys
+import time
 import urllib.request
+import getpass
+import connect
 import daemon
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='auto-connect')
-    parser.add_argument('-p', '--pidfile', metavar='PIDFILE', type=str, default='/tmp/auto-connect.pid', help='set the pid file.')
-    parser.add_argument('-l', '--logfile', metavar='LOGFILE', type=str, default='/tmp/auto-connect.log', help='set the log file.')
-    parser.add_argument('-d', '--daemon', choices=['start', 'stop', 'restart'], nargs='?', const='start', help='set if works in daemon mode.')
+    parser.add_argument('-p', '--pidfile', metavar='PIDFILE', type=str, default='/tmp/auto-connect.pid',
+                        help='set the pid file.')
+    parser.add_argument('-l', '--logfile', metavar='LOGFILE', type=str, default='/tmp/auto-connect.log',
+                        help='set the log file.')
+    parser.add_argument('-i', '--intervel', metavar='INTERVEL', type=int, default=1800,
+                        help='set the intervel time.')
+    parser.add_argument('-u', '--id', metavar='ID', type=str, help='set the id.')
+    parser.add_argument('-w', '--password', metavar='PASSWORD', type=str, help='set the password.')
+    parser.add_argument('-d', '--daemon', choices=['start', 'stop', 'restart'], nargs='?', const='start',
+                        help='set if works in daemon mode.')
     args = parser.parse_args()
     if args.daemon:
         if args.daemon == 'start':
@@ -38,17 +46,26 @@ if __name__ == '__main__':
         if os.path.exists(args.pidfile):
             raise RuntimeError('Already running')
 
+    if args.id:
+        id = args.id
+        if args.password:
+            password = args.password
+        else:
+            password = getpass.getpass('Input your password:')
+    else:
+        id = input('Input your id:')
+        password = getpass.getpass('Input your password:')
+
+
     while True:
         url = 'http://www.baidu.com'
         try:
-            respense = urllib.request.urlopen(url, timeout=10)
+            respense = urllib.request.urlopen(url, timeout=1)
             print(time.ctime(), 'all right')
         except Exception as e:
-            # print(str(e.reason) == 'timed out')
-            if e.reason and str(e.reason) == 'timed out':
-                print(time.ctime(), 'reconnect')
-                try:
-                    os.system('~/connect.sh connect domestic {0} {1}'.format('id', 'passwd'))
-                except Exception as e:
-                    print(e)
-        time.sleep(3600)
+            print(time.ctime(), 'reconnect')
+            try:
+                connect.connect(id, password)
+            except Exception as e:
+                print(e)
+        time.sleep(args.intervel)
